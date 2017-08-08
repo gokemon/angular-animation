b87d9ae199b782f37bc0a5f466bcd22a626b84d6
gokemon <michael.of.gokey@gmail.com>
2017-08-07 23:38:42 -0400

    working on old AngularJS and CSS Animation

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# angular-animation
+# Angular-Animation
+
+# ng-class & CSS animation
+
+### Objectives
+1. Apply classes using 3 different ng-class variations
+1. Change classes dynamically based on changing variables
+1. Apply a CSS class using transition for subtle animation
+
+### Preparation
+- Create a basic Angular app
+- Be awesome at CSS
+- Explain the use case for CSS classes
---
+++ app/js/awardsController.js
@@ -0,0 +1,24 @@
+angular.module('Awwward', [])
+    .controller('awardsController', AwardsController);
+
+
+function AwardsController() {
+    var vm = this; // added to control scope
+
+    this.newWinner = {};
+    this.winners = [
+        { name: "Michael Gokey" },
+        { name: "George Washington" },
+        { name: "Ben Frank" },
+        { name: "Gen Bush" },
+        { name: "Donny Thumper" },
+        { name: "Harry Winston" },
+        { name: "Andrew Crater" }
+    ];
+    this.createNewWinner = createNewWinner;
+
+    function createNewWinner() {
+        this.winners.push(this.newWinner);
+        this.newWinner = {};
+    }
+}
